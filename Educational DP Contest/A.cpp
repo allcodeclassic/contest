cd3bf9bf9374da53a2ed8094956825d1e9d4f8ad
hmtrung2305 <61732256+allcodeclassic@users.noreply.github.com>
2023-11-25 15:43:18 +0700

    Add files via upload

---
+++ Educational DP Contest/A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    int numStone;
+    std::cin >> numStone;
+    std::vector<int> heightStone(numStone);
+    for (int index = 0; index < numStone; index++) {
+        std::cin >> heightStone[index];
+    }
+    std::vector<int> dp(numStone, 1e9);
+    dp[0] = 0;
+    for (int index = 1; index < numStone; index++) {
+        dp[index] = dp[index - 1] + abs(heightStone[index] - heightStone[index - 1]);
+        if (index > 1) {
+            dp[index] = min(dp[index], dp[index - 2] + abs(heightStone[index] - heightStone[index - 2]));
+        }
+    }
+    cout << dp[numStone - 1];
+    return 0;
+}
